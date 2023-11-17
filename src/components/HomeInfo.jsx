30788075838173d27679960a4c6d67cd7c920889
okellomano <onyandookello@gmail.com>
2023-11-17 18:17:04 +0300

    HomeInfo

---
+++ src/components/HomeInfo.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { arrow } from '../assets/icons'
+
+// Info box for each stge
+const InfoBox = ({ text, link, btnText }) => (
+    <div className='info-box'>
+        <p className='font-medium sm:text-xl text-center'>{text}</p>
+        <Link to={link} className='neo-brutalism-white neo-btn'>
+            {btnText}
+            <img src={arrow} className='w-4 h-4 object-contain'/>
+        </Link>
+    </div>
+)
+
+const renderContent = {
+    1: (
+        <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
+            Hi, I am <span className='font-semibold'>Edward</span> 👋
+            <br />
+            A DevSecOps Engineer
+        </h1>
+    ),
+    2: (
+        <InfoBox 
+            text='Worked with many companies and picked up many skills along the way'
+            link='/about'
+            btnText='Learn More'
+        />
+    ),
+    3: (
+        <InfoBox 
+            text='Led multiple projects to success over the years. Curious about the impact?'
+            link='/projects'
+            btnText='View my portfolio'
+        />
+    ),
+    4: (
+        <InfoBox 
+            text='Need a project done? I am just a few keystrokes away.'
+            link='/contact'
+            btnText="Let's Talk"
+        />
+    )
+}
+
+
+const HomeInfo = ({ currentStage }) => {
+  return renderContent[currentStage] || null
+}
+
+export default HomeInfo
